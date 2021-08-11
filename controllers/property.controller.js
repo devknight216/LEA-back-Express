20e9dc5ee09c054b5b93a4f0c87514fe2a7bd5d3
devknight216 <devknight216@gmail.com>
2021-08-11 12:58:10 +0300

    update property model schema

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -37,7 +37,7 @@ function list(req, res, next) {
 
 function remove(req, res, next) {
   req.property.remove(() => {
-    res.json(req.property)
+    res.json({ message: 'Property removed successfully'})
   })
   .catch(next)
 }
