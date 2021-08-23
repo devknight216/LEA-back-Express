d5f78d528027023e8396554c28b12f109834a083
devknight216 <devknight216@gmail.com>
2021-08-23 14:37:51 +0300

    Property api findAll and findById

--- routes/api/property.route.js
+++ routes/api/property.route.js
@@ -7,21 +7,20 @@ const roleMiddleware = require('../../middleware/roles.middleware');
 const router = express.Router();
 
 router.route('/')
-  .get(passport.authenticate('jwt', {session: false}), roleMiddleware('INDEX'), propertyController.list)
+  .get(propertyController.list)
   .post(passport.authenticate('jwt', {session: false}), roleMiddleware('CREATE'), propertyController.create)
 
 router.route('/images')
   .post(passport.authenticate('jwt', {session: false}), roleMiddleware('CREATE'), propertyController.uploadImage)
 
 router.route('/images/:propertyId')
-  .get(passport.authenticate('jwt', {session: false}), roleMiddleware('READ'), propertyController.getPropertyImages)
+  .get(propertyController.getPropertyImages)
   .post(passport.authenticate('jwt', {session: false}), roleMiddleware('DELETE'), propertyController.removePropertyImages)
 
 router.route('/search')
   .post(propertyController.searchProperties)
 
 router.route('/:propertyId')
-  .get(passport.authenticate('jwt', {session: false}), roleMiddleware('READ'), propertyController.read)
   .put(passport.authenticate('jwt', {session: false}), roleMiddleware('UPDATE'), propertyController.update)
   .delete(passport.authenticate('jwt', {session: false}), roleMiddleware('DELETE'), propertyController.remove)
 
