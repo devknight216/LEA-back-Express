3bfebece48438c0c91c99942a0902d4606c408ad
devknight216 <devknight216@gmail.com>
2021-08-30 12:51:29 +0300

    Payment API with Stripe Draft

--- models/reservation.model.js
+++ models/reservation.model.js
@@ -8,7 +8,9 @@ const ReservationSchema = new Schema({
   guestNum: { type: Number },
   price: { type: Number },
   total: { type: Number },
-  guest: { type: Schema.ObjectId, ref: 'User' }
+  guest: { type: Schema.ObjectId, ref: 'User' },
+  paymentIntentId: { type: String },
+  paymentStatus: { type: String }
 })
 
 module.exports = mongoose.model('Reservation', ReservationSchema)
